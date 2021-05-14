d599b3535c50b79c39e4524bb06e827162a12e0a
Tom <98tmurphy@gmail.com>
2021-05-14 16:06:37 -0500

    cliccking?

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@
 
 
 typedef struct fileData {
+    std::string path;
+    std::string fileNameString;
     int iconIndex;
     SDL_Rect iconRect;
     SDL_Texture* fileName;
@@ -68,17 +70,32 @@ int main(int argc, char **argv)
         SDL_WaitEvent(&event);
         switch(event.type) 
         {
-            
-            
-
-
-
-
-
-
-
-
-
+            case SDL_MOUSEBUTTONDOWN:
+            //check to see if the mouse is clicking on a file
+                for(int i = 0; i < data.fileList.size(); i++){
+                    if (event.button.button == SDL_BUTTON_LEFT &&
+                        event.button.y >= data.fileList.at(i)->fileNameRect.y &&
+                        event.button.y <= data.fileList.at(i)->fileNameRect.y + data.fileList.at(i)->fileNameRect.h){
+                            
+                            if(getFileType(data.fileList.at(i)->path) == "directory"){
+                                // see if the mouse is clicking on a directory
+                                SDL_DestroyRenderer(renderer);
+                                SDL_DestroyWindow(window);
+                                SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
+                                if(data.fileList.at(i)->fileNameString == ".."){
+                                    //checks if the directory is the parent directory
+                                    std::cout << "FOUND THE FUCKER" << std::endl;
+                                    data.fileList.at(i)->path = data.fileList.at(i)->path + "/..";
+                                    std::cout << data.fileList.at(i)->path << std::endl;
+                                }
+                                
+                                
+                                renderDirectory(renderer, data.fileList.at(i)->path.c_str(), &data);
+                                render(renderer, &data);
+                                break;
+                            }
+                        }
+                }
         }
     }
 
@@ -132,6 +149,7 @@ void renderDirectory(SDL_Renderer *renderer, std::string directory, AppData *dat
         fileList.push_back(fileEntry->d_name);
     }
 
+
     std::sort(fileList.begin(), fileList.end());
     fileList.erase(fileList.begin());
 
@@ -140,6 +158,9 @@ void renderDirectory(SDL_Renderer *renderer, std::string directory, AppData *dat
         fileData *file = new fileData();
         std::string filePath = directory + "/" + fileList.at(i);
         std::cout << filePath << std::endl;
+        file->path = filePath;
+        file->fileNameString = fileList.at(i);
+
         file->fileType = getFileType(filePath);
         file->iconIndex = 0;
         file->iconRect.x = 10;
@@ -147,11 +168,13 @@ void renderDirectory(SDL_Renderer *renderer, std::string directory, AppData *dat
         file->iconRect.w = 25;
         file->iconRect.h = 25;
         SDL_Color color = { 0, 0, 0 };
+
         SDL_Surface *phrase_surf = TTF_RenderText_Solid(data_ptr->font, fileList.at(i).c_str(), color);
         file->fileName = SDL_CreateTextureFromSurface(renderer, phrase_surf);
         SDL_FreeSurface(phrase_surf);
         file->fileNameRect.x = 50;
         file->fileNameRect.y = 25 + (i * 25);
+        file->fileNameRect.h = 50;
         SDL_QueryTexture(file->fileName, NULL, NULL, &(file->fileNameRect.w), &(file->fileNameRect.h));
 
 
@@ -160,28 +183,27 @@ void renderDirectory(SDL_Renderer *renderer, std::string directory, AppData *dat
 
 
         if (file->fileType == "executable") {
-            std::cout << "exe" << std::endl;
+            ///std::cout << "exe" << std::endl;
             img_surf = IMG_Load("resrc/exe-extension.png");
         } else if (file->fileType == "directory") {
-            std::cout << "dir" << std::endl;
+            //std::cout << "dir" << std::endl;
             img_surf = IMG_Load("resrc/directory-extension.webp");
         } else if (file->fileType == "image") {
-            std::cout << "img" << std::endl;
+            //std::cout << "img" << std::endl;
             img_surf = IMG_Load("resrc/img-extension.png");
         } else if (file->fileType == "video") {
-            std::cout << "vid" << std::endl;
+            //std::cout << "vid" << std::endl;
             img_surf = IMG_Load("resrc/video-extension.png");
         } else if (file->fileType == "other") {
-            std::cout << "other" << std::endl;
+            //std::cout << "other" << std::endl;
             img_surf = IMG_Load("resrc/other-extension.png");
         } else if (file->fileType == "code") {
-            std::cout << "code" << std::endl;
+            //std::cout << "code" << std::endl;
             img_surf = IMG_Load("resrc/code-extension.png");
         }
 
         file->iconTexture = SDL_CreateTextureFromSurface(renderer, img_surf);
         SDL_FreeSurface(img_surf);
-        //SDL_QueryTexture(file->iconTexture, NULL, NULL, &(file->iconRect.w), &(file->iconRect.h));
         data_ptr->fileList.push_back(file);
 
     }
@@ -239,7 +261,3 @@ std::string getFileType(std::string file) {
 
 
 }
-
-
-
-
